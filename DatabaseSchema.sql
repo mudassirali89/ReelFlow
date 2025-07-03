069eac01f3004900f80f50a6bd664fb0cc2fccfe
mudassirali89 <mdmudassirali27@gmail.com>
2025-07-03 22:16:28 +0530

    movie table added

--- DatabaseSchema.sql
+++ DatabaseSchema.sql
@@ -1,1 +1,20 @@
+create database ReelflowDB;
+go
 
+use ReelflowDB;
+go
+
+create table Movies(
+MovieId int identity(1,1) primary key,
+MovieName nvarchar(100) not null,
+MovieInfo nvarchar(max),
+ReleaseYear int,
+Duration int,
+FilePath nvarchar(500),
+PosterPath nvarchar(500),
+DirectorId int,
+GenreId int
+foreign key (DirectorId) references Directors(DirectorId),
+foreign key (GenreId) references Genres(GenreId)
+);
+go
