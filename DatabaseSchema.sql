d1ef4a03779cb6eeeada768cad4d65121735c62b
anjali1530 <anjaliagrawal91716@gmail.com>
2025-07-04 14:12:41 +0530

    added movieactorgenre table in databaseschema

--- DatabaseSchema.sql
+++ DatabaseSchema.sql
@@ -18,3 +18,19 @@ foreign key (DirectorId) references Directors(DirectorId),
 foreign key (GenreId) references Genres(GenreId)
 );
 go
+
+CREATE TABLE Genres (
+    GenreId INT PRIMARY KEY IDENTITY(1,1),
+    GenreName VARCHAR(100) NOT NULL
+);
+go
+
+
+CREATE TABLE MovieActors (
+    MovieId INT NOT NULL,
+    ActorId INT NOT NULL,
+    PRIMARY KEY (MovieId, ActorId),
+    FOREIGN KEY (MovieId) REFERENCES Movies(MovieId),
+    FOREIGN KEY (ActorId) REFERENCES Actors(ActorId)
+);
+go
