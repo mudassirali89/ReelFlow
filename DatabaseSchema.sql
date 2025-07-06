7be874e57698f2fbddf6b34b443b18d610960178
anjali1530 <anjaliagrawal91716@gmail.com>
2025-07-06 23:42:59 +0530

    Revert accidental merge of anjali branch

--- DatabaseSchema.sql
+++ DatabaseSchema.sql
@@ -18,7 +18,6 @@ foreign key (DirectorId) references Directors(DirectorId),
 foreign key (GenreId) references Genres(GenreId)
 );
 go
-
 --Directors Table
 CREATE TABLE Directors(
 DirectorId int primary key identity(1,1),
@@ -26,21 +25,3 @@ DirectorName nvarchar(255) NOT NULL,
 DirectorBio nvarchar(max)
 );
 GO
-=======
-
-CREATE TABLE Genres (
-    GenreId INT PRIMARY KEY IDENTITY(1,1),
-    GenreName VARCHAR(100) NOT NULL
-);
-go
-
-
-CREATE TABLE MovieActors (
-    MovieId INT NOT NULL,
-    ActorId INT NOT NULL,
-    PRIMARY KEY (MovieId, ActorId),
-    FOREIGN KEY (MovieId) REFERENCES Movies(MovieId),
-    FOREIGN KEY (ActorId) REFERENCES Actors(ActorId)
-);
-go
-
